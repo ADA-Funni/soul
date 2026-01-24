3476718b32397278d06daf155410d4aabcc707af
ADA-Funni <codingking263@gmail.com>
2026-01-24 23:02:31 +0000

    Update Save.hx

--- src/soul/save/Save.hx
+++ src/soul/save/Save.hx
@@ -7,6 +7,9 @@ import flixel.util.FlxSave;
  * A helper class for accessing FlxG.save.data.
  */
 class Save {
+    /**
+     * The file path for your save folder. (in %appdata%/Roaming on Windows)
+     */
     public static var path(get, never):String;
         inline static function get_path():String {
             final company:String = FlxG.stage.application.meta.get('company');
@@ -15,17 +18,27 @@ class Save {
 		    return '$company/${validate(file)}';
         }
 
-    inline public static function get(name:String):Dynamic {
+    /**
+     * Gets field `name` from your save file.
+     * @param name String
+     * @return Null<Dynamic>
+     */
+    inline public static function get(name:String):Null<Dynamic> {
         return Reflect.getProperty(FlxG.save.data, name) ?? null;
     }
 
+    /**
+     * Sets field `name` to `value` in your save file.
+     * @param name String
+     * @param value Dynamic
+     */
     inline public static function set(name:String, value:Dynamic) {
         Reflect.setProperty(FlxG.save.data, name, value);
     }
 
     /**
-     * Fuck yeah, public save validation!
-     * @param str 
+     * Formats `str` to always work as a save directory.
+     * @param str String
      * @return String
      */
     inline public static function validate(str:String):String {
